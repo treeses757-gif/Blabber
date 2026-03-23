f78d7aa4f168b4155463c0d6d8bf14c9abfd7353
treeses757-gif <treeses757@gmail.com>
2026-03-23 20:52:06 +0500

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -16,3 +16,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const app = app;
